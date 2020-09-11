f05e9780cb0a0b9fcdf559ce8546424c9817dd44
Enes <71134553+onewayenes@users.noreply.github.com>
2020-09-12 00:04:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,34 +5,11 @@ const container = document.querySelector('#container')
 var login_button = document.querySelector('#login-button')
 var register_button = document.querySelector('#register-button')
 
-var login_html = `
-    <h3>login</h3>
-    <form action="">
-        <input type="text" id="nick" required placeholder="nickname">
-        <input type="password" id="password" required placeholder="password">
-        <button type="submit" id="login-button">login</button>
-    </form>`
-var register_html = `
-    <h3>register</h3>
-    <form action="">
-        <input type="text" id="nick" required placeholder="nickname">
-        <input type="password" id="password" required placeholder="password">
-        <button type="submit" id="register-button">register</button>
-    </form>`
-
-document.querySelector('#login').addEventListener('click', () => {
-    container.innerHTML = login_html
-})
-
-document.querySelector('#register').addEventListener('click', () => {
-    container.innerHTML = register_html
-})
-
 register_button.addEventListener('click', () =>{
     event.preventDefault()
 
-    let nick = document.querySelector('#nick').value
-    let password = document.querySelector('#password').value
+    let nick = document.querySelector('#nick2').value
+    let password = document.querySelector('#password2').value
 
     var data = {}
 
@@ -45,12 +22,12 @@ register_button.addEventListener('click', () =>{
     xhr.setRequestHeader('Content-type','application/json; charset=utf-8')
 
     xhr.onload = () => {
-        if (xhr.readyState === 4 && xhr.status === "201") {
+        if (xhr.readyState === 4 && xhr.status === 200) {
             alert('You\'ve succesfully registered.')
-            //show login page
+            console.log(xhr.responseText)
         } else {
             alert('There is a problem.')
-            //problem stuff
+            console.log(xhr.readyState)
         }
     }
 
